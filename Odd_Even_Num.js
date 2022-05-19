53e7f9be07dacb176d40faef780e1da4aea200b7
Pranil Anap <anappranil@gmail.com>
2022-05-19 17:24:07 +0530

    ODD EVEN Number

---
+++ Odd_Even_Num.js
@@ -0,0 +1,13 @@
+let num1 = 25;
+
+let num3 = num1 % 2
+
+if (num3 === 0){
+  console.log("The number is EVEN");
+}
+else if(num3 > 0){
+  console.log("The number is ODD");
+}
+else{
+  console.log("UNKNOWN");
+}

862a07ace766af55cdcd413adb91219c6deaf94c
Pranil Anap <anappranil@gmail.com>
2022-05-24 20:08:27 +0530

    Perfect Square

---
+++ Perfect_Square.js
@@ -0,0 +1,10 @@
+let n = 25;
+
+let isperfectsquare = Math.floor(Math.sqrt(n));
+if(isperfectsquare * isperfectsquare == n){
+  console.log("This is Perfect Square")
+}
+else{
+  console.log("This is not Perfect Square")
+  
+}

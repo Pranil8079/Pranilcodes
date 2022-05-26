fa4f72d6237bbb2128a82e97ade98bee7e09fa4d
Pranil Anap <anappranil@gmail.com>
2022-05-26 17:24:40 +0530

    Austro guru

---
+++ Astroguru.js
@@ -0,0 +1,9 @@
+function astroGuru(job, geolocation, spouse, numKids) {
+    var future = 'You will be a ' + job + ' in ' + geolocation + ' and married to ' +
+    spouse + ' with ' + numKids + ' kids.';
+    console.log(future);
+  }
+  
+  astroGuru('Doctor', 'Pune', 'Akshay', 3);
+  astroGuru('Software Dev', 'Mumbai', 'Alia', 2);
+  astroGuru('Teacher', 'Rahuri', 'Deepak', 1);
